76432eb5c19bdb682c0f3f2eca13e3e300065953
Diana Rodríguez Espiricueta <170010@upslp.edu.mx>
2020-09-09 22:06:44 -0500

    Programa de Conjuntos

---
+++ 170010_Conjuntos.py
@@ -0,0 +1,12 @@
+def transformarConj():
+  A = (1,2,3)
+  conjuntoA = set (A) #Set () Transforma o convierte a un conjunto
+  print("The set is:", conjuntoA)
+  B=(1,2,3,4,5)
+  conjuntoB = set (B)
+  print("The set B es:", conjuntoB)
+  C= 'Hola mundo'
+  conjuntoC = set (C)
+  print("The set C is :", conjuntoC)
+
+transformarConj()
